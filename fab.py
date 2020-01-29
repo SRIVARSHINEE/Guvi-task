c1198a040b3bd99dcbe4b659c01997999d774bd2
SRIVARSHINEE <60417262+SRIVARSHINEE@users.noreply.github.com>
2020-01-29 12:33:02 +0530

    Create fab.py

---
+++ fab.py
@@ -0,0 +1,12 @@
+a=int(input("enter the number:"))
+first=0
+second=1
+if(a<=0):
+ print("the required series",f)
+ else:
+ print(f,s,end="")
+ for i in range(2,a):
+ next=first+second
+ print(next,end="")
+ first=second
+ second=next
