b8d76949deaae1594512047acec065840cf38810
SRIVARSHINEE <60417262+SRIVARSHINEE@users.noreply.github.com>
2020-01-29 12:13:41 +0530

    Create biling.py

---
+++ biling.py
@@ -0,0 +1,6 @@
+a=input("enter the starting point");
+b=input("enter the ending point");
+str=("select a vehicle");
+import random
+list1 = ['car','moto','auto']  
+print("random item from list is: ", random.choice(list1))
